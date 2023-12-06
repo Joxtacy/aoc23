50b9ce8b4b479aa395562fc815752b6bc295310d
Jesper Hasselquist <jesper.hasselquist@telavox.se>
2023-12-07 00:05:47 +0100

    feat(day-06): part 2 solved

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -2,11 +2,13 @@ fn main() {
     let input = include_str!("input.txt");
 
     let res1 = part_1(input);
+    let res2 = part_2(input);
 
     println!("Part 1: {}", res1);
+    println!("Part 2: {}", res2);
 }
 
-fn distance(time_pressed: u32, race_time: u32) -> u32 {
+fn distance(time_pressed: u64, race_time: u64) -> u64 {
     time_pressed * (race_time - time_pressed)
 }
 
@@ -33,9 +35,6 @@ fn part_1(input: &str) -> u64 {
         .map(|t| t.parse().expect("should be a number"))
         .collect();
 
-    dbg!(&times);
-    dbg!(&distances);
-
     let mut total = 1;
     for i in 0..times.len() {
         let race_time = times[i];
@@ -43,8 +42,8 @@ fn part_1(input: &str) -> u64 {
 
         let mut nbr_beaten = 0;
         for time_pressed in 1..race_time {
-            let distance = distance(time_pressed, race_time);
-            if distance > distance_to_beat {
+            let distance = distance(time_pressed as u64, race_time as u64);
+            if distance > distance_to_beat as u64 {
                 nbr_beaten += 1;
             }
         }
@@ -52,6 +51,44 @@ fn part_1(input: &str) -> u64 {
     }
 
     total
+}
+
+fn part_2(input: &str) -> u64 {
+    let mut lines = input.lines();
+    let time_row = lines.next().expect("should have a line of times");
+    let distance_row = lines.next().expect("should have a line of distances");
+    drop(lines);
+
+    let race_time: u64 = time_row
+        .split_once("Time:")
+        .expect("should have time")
+        .1
+        .trim()
+        .split_whitespace()
+        .collect::<Vec<&str>>()
+        .join("")
+        .parse()
+        .expect("should be a number");
+
+    let distance_to_beat: u64 = distance_row
+        .split_once("Distance:")
+        .expect("should have distance")
+        .1
+        .trim()
+        .split_whitespace()
+        .collect::<Vec<&str>>()
+        .join("")
+        .parse()
+        .expect("should be a number");
+
+    let mut nbr_beaten = 0;
+    for time_pressed in 1..race_time {
+        let distance = distance(time_pressed, race_time);
+        if distance > distance_to_beat {
+            nbr_beaten += 1;
+        }
+    }
+    nbr_beaten
 }
 
 #[cfg(test)]
@@ -66,5 +103,15 @@ Distance:  9  40  200";
         let res = part_1(input);
 
         assert_eq!(res, 288);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = "Time:      7  15   30
+Distance:  9  40  200";
+
+        let res = part_2(input);
+
+        assert_eq!(res, 71503);
     }
 }
